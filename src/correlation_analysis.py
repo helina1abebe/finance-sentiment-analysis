8ba32448d083fe228eb1dbd5046f531b5acf1bb9
Helina Abebe Bekele <helina1abebe@gmail.com>
2025-06-03 21:55:47 +0300

    Add correlation analysis functions for stock and news data

---
+++ src/correlation_analysis.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def align_dates(stock_df, news_df):
+    # Code for date alignment
+    ...
+
+def calculate_correlation(aligned_df):
+    # Code for correlation analysis
+    ...
